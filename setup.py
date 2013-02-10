8b5e141d2934567fcf45d217890f6a0b2f58587e
David Danier <david.danier@team23.de>
2013-02-10 14:47:25 +0100

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = "fabdeploit",
+    version = "0.1.0",
+    description = 'fabric utilities for git based deployments',
+    author = 'David Danier',
+    author_email = 'david.danier@team23.de',
+    #url = 'https://github.com/ddanier/fabdeploit',
+    #long_description=open('README.rst', 'r').read(),
+    packages = [
+        'fabdeploit',
+    ],
+    package_data = {
+    },
+    install_requires = [
+        'fabric >=1.4',
+        'GitPython >=0.3.0',
+    ],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Web Environment',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Utilities'
+    ],
+    zip_safe=False,
+)
+
