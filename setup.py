aea62cf512a593451b2823280d395e178b6cd2c1
David Danier <david.danier@team23.de>
2015-02-23 18:28:56 +0100

    v0.10.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = "fabdeploit",
-    version = "0.10.0",
+    version = "0.10.1",
     description = 'fabric utilities for git based deployments',
     author = 'David Danier',
     author_email = 'david.danier@team23.de',
